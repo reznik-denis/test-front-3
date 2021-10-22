54747c4517d298188d0091fde8c0c7d2bae0391a
Reznik Denis <63472391+reznik-denis@users.noreply.github.com>
2021-10-22 13:46:35 +0300

    add task2

---
+++ src/Components/TableBody.js
@@ -0,0 +1,67 @@
+import { useState } from 'react';
+
+const goods = [
+    { id: 1, good: 'Красный носок' },
+    { id: 2, good: 'Синий носок' },
+    { id: 3, good: 'Бесцветный носок' },]
+
+function TableBody() {
+    const [number, setNumber] = useState(0);
+    const [price, setPrice] = useState(0);
+    const [total, setTotal] = useState(0);
+
+    const handleChangeNumber = event => {
+        setNumber(event.currentTarget.value);
+    };
+
+    const handleChangePrice = event => {
+        setPrice(event.currentTarget.value);
+    };
+    
+    const handleSubmit = event => {
+        event.preventDefault();
+        setTotal(number * price);
+    }
+        // const reset = () => {
+    //     setPrice(0);
+    //     setNumber(0);
+    // };
+
+    return (<tbody>
+        {goods.map(({ id, good }) => (
+            <tr key={id} >
+                <td className="textAlign">{good}</td>
+                <td>
+                    <form onSubmit={handleSubmit}>
+                        <input
+                            className="textAlign"
+                            type="number"
+                            name="number"
+                            value={number}
+                            onChange={handleChangeNumber}
+                        />
+                    </form>
+                </td>
+                <td>
+                    <form>
+                        <input
+                            className="textAlign"
+                            type="number"
+                            name="price"
+                            value={price}
+                            onChange={handleChangePrice}
+                        />
+                    </form>
+                </td>
+                <td>
+                    {total}
+                </td>
+            </tr>))}
+         <tr className="textColor">
+            <td className="textAlign" colSpan="3">Общая стоимость всех товаров</td>
+            <td>20</td>
+        </tr>
+        </tbody>)
+};
+
+export default TableBody;
---
+++ src/Components/TableHead.js
@@ -0,0 +1,12 @@
+function TableHead() {
+  return (<thead>
+      <tr>
+            <th>Название товара</th>
+            <th>Количество</th>
+            <th>Стоимость 1 ед.</th>
+            <th>Общая <br /> стоимость</th>
+          </tr>
+    </thead>)
+};
+
+export default TableHead;
